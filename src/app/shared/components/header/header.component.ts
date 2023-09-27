b40c721033558f31d05055f9959c55fce508e00f
Tai Vo <tantaivo74@gmail.com>
2023-09-27 14:06:06 +0700

    chore: replace sass -> scss

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
